712222fbdd761448b1e3772bbb74190bf33dc11a
mding5692 <m.ding5692@gmail.com>
2019-10-11 11:14:59 -0400

    Able to play multiple notes

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-// let ctx = new AudioContext();
-// let osc = ctx.createOscillator();
-// osc.connect(ctx.destination)
-// window.onmousemove = event => osc.frequency.value = event.clientX
-// osc.start()
+
--- synth.js
+++ synth.js
@@ -1,24 +1,106 @@
 import AudioKeys from 'audiokeys';
+import Tone from 'tone';
+import { scale } from "@tonaljs/scale";
+import { sample } from 'lodash';
+import webmidi from "webmidi";
 
-let ctx = new AudioContext();
-let osc = ctx.createOscillator();
+// LESSON 0
 
-osc.start()
+// let ctx = new AudioContext();
+// let osc = ctx.createOscillator();
+// osc.connect(ctx.destination)
+// window.onmousemove = event => osc.frequency.value = event.clientX
+// osc.start()
 
-var keyboard = new AudioKeys();
+// LESSON 1
 
-var oscMap = {};
+// let ctx = new AudioContext();
+// let osc = ctx.createOscillator();
 
-keyboard.down(note => {
-    let osc = ctx.createOscillator();
-    osc.frequency.value = note.frequency;
-    osc.connect(ctx.destination)
-    osc.start();
+// osc.start()
 
-    oscMap[note.frequency] = osc;
-});
+// var keyboard = new AudioKeys();
 
-keyboard.up(note => {
-    let osc = oscMap[note.frequency];
-    osc.stop();
+// var oscMap = {};
+
+// keyboard.down(note => {
+//     let osc = ctx.createOscillator();
+//     osc.frequency.value = note.frequency;
+//     osc.connect(ctx.destination)
+//     osc.start();
+
+//     oscMap[note.frequency] = osc;
+// });
+
+// keyboard.up(note => {
+//     let osc = oscMap[note.frequency];
+//     osc.stop();
+// });
+
+
+// LESSON 2
+
+// var synth = new Tone.Synth().toMaster()
+// let keyboard = new AudioKeys();
+
+// keyboard.down(note => {
+//     synth.triggerAttackRelease(note.frequency, '8n');
+// });
+
+// LESSON 3
+
+// let scaleType1 = 'c3 hirajoshi';
+// let scaleType2 = 'c4 hirajoshi';
+// let { notes: n1 } = scale(scaleType1);
+// let { notes: n2 } = scale(scaleType2);
+
+// let notes = [...n1, ...n2];
+
+// var synth = new Tone.PolySynth(6, Tone.Synth, {
+//     oscillator: {
+//       type: "sine"
+//     }
+//   }).toMaster();
+// let keyboard = new AudioKeys();
+
+// keyboard.down(note => {
+//     let currNote = sample(notes);
+//     synth.triggerAttackRelease(currNote, "8n");
+// });
+
+// LESSON 4
+
+// var synth = new Tone.PolySynth(6, Tone.Synth, {
+//     oscillator: {
+//       type: "sine"
+//     }
+//   }).toMaster();
+
+// let scaleType1 = 'c3 hirajoshi';
+// let scaleType2 = 'c4 hirajoshi';
+// let { notes: n1 } = scale(scaleType1);
+// let { notes: n2 } = scale(scaleType2);
+
+// let notes = [...n1, ...n2];
+
+// var loop = new Tone.Loop(function(time) {
+//     //triggered every eighth note.
+//     let note = sample(notes);
+//     synth.triggerAttackRelease(note, "8n", time);
+// }, "32n").start(0);
+
+// Tone.Transport.start();
+
+// LESSON 5
+
+webmidi.enable(function (err) {
+
+    if (err) {
+        console.log("WebMidi could not be enabled.", err);
+    } else {
+        console.log("WebMidi enabled!");
+        console.log(webmidi.inputs);
+        console.log(webmidi.outputs);
+    }
+    
 });
